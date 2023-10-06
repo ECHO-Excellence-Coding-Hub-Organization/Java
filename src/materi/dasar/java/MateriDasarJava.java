b954f491dfce5efcd450cb703113917cf7e62cd7
Sabardi <Sabardibahari@gmail.com>
2023-10-07 00:41:12 +0800

    firs komit

---
+++ src/materi/dasar/java/MateriDasarJava.java
@@ -0,0 +1,13 @@
+package materi.dasar.java;
+
+public class MateriDasarJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hallo");
+    }
+    
+}
