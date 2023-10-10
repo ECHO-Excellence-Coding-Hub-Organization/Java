656541bfbe8badb16a5bd3d98182947be136f34a
Sabardi <Sabardibahari@gmail.com>
2023-10-10 15:40:17 +0800

    add file

---
+++ src/materi/dasar/java/Ternary.java
@@ -0,0 +1,14 @@
+package materi.dasar.java;
+
+public class Ternary {
+    public static void main(String[] args) {
+        boolean suka = true;
+        int angka = 10;
+
+        // System.out.println(suka ?  "suka" : "tidak");
+        System.out.println(angka > 20 ? "iya" : "tidak donk");
+
+        int a = 10;
+        System.out.println(a);
+    }
+}
